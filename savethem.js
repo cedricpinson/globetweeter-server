a2727c9c1ec16d6990ed489c42b04803534964a3
Cedric Pinson <cedric.pinson@plopbyte.net>
2010-12-26 16:01:23 +0000

    add a scripts to save template

---
+++ savethem.js
@@ -0,0 +1,35 @@
+var TwitterNode = require('twitter-node').TwitterNode;
+var twit = new TwitterNode({
+    user: 'statistweets', 
+    password: 'TwitterPsc80',
+    locations: [ -180, -90, 180, 90 ]
+});
+
+twit.addListener('error', function(error) {
+    console.log(error.message);
+}).addListener('tweet', function(tweet) {
+    //console.log("@" + tweet.user.screen_name + ": " + tweet.text);
+    //console.log(tweet.toString());
+    console.log(JSON.stringify(tweet) + ",");
+}).addListener('end', function(resp) {
+    console.log("wave goodbye... " + resp.statusCode);
+}).stream();
+
+var express = require('express');
+var app = express.createServer(); 
+app.use(express.staticProvider(__dirname + '/public'));
+app.listen(22048, "192.168.100.231");
+
+
+var io = require('socket.io');   
+var socket = io.listen(app);
+socket.on('connection', function(client){ 
+    var tweet2socket = function(twit) {
+	client.send(twit);
+    };
+    twit.addListener('tweet', tweet2socket);	
+    client.on('disconnect', function () {
+	twit.removeListener('tweet', tweet2socket);
+    });
+
+});
